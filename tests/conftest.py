dddbb2e174b28c15a96edd927379984a4371cf3e
pmintzas <panagiotis.mintzas@justice.gov.uk>
2022-05-25 10:04:08 +0000

    Fix jars location in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,8 @@ def spark():
     conf = SparkConf()
 
     conf.set("spark.sql.shuffle.partitions", "1")
-    conf.set("spark.jars.ivy", "/home/jovyan/.ivy2/")
+    conf.set("spark.jars",
+        "hmpps_person_match_score/jars/scala-udf-similarity-0.0.8.jar,hmpps_person_match_score/jars/graphframes-0.8.0-spark3.0-s_2.12.jar",)
     conf.set("spark.driver.memory", "4g")
     conf.set("spark.sql.shuffle.partitions", "24")
 
