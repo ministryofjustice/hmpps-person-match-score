17f013bd28d39466735e11a7354f966a602bc90e
John Evans <john.evans@digital.justice.gov.uk>
2023-05-15 08:53:26 +0100

    Pic 1446 general improvements (#20)
    * 📝 Update documentation
    
    * 🔊 Add additional telemetry and fix logging

--- hmpps_person_match_score/__init__.py
+++ hmpps_person_match_score/__init__.py
@@ -1,36 +1,21 @@
 import os
 import platform
 import sys
-from logging.config import dictConfig
+import logging
 
 import flask
-from . import db, match, ai
+from . import db, match
+from hmpps_person_match_score.app_insights import app_insights_logger, logger
 
 def create_app(test_config=None):
-    ai_logger_instance = ai.instance
     try:
-        ai_logger_instance.get_logger(__name__).info("Starting hmpps-person-match-score using Python %s on %s" % (" ".join(sys.version.split(" ")[:1]), platform.platform()))
-
-        dictConfig({
-            'version': 1,
-            'formatters': {'default': {
-                'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-            }},
-            'handlers': {'wsgi': {
-                'class': 'logging.StreamHandler',
-                'stream': 'ext://flask.logging.wsgi_errors_stream',
-                'formatter': 'default'
-            }},
-            'root': {
-                'level': 'INFO',
-                'handlers': ['wsgi']
-            }
-        })
-
+        logging.basicConfig(level=logging.INFO)
 
         # create and configure the app
         app = flask.Flask(__name__, instance_relative_config=True)
-        ai_logger_instance.initRequestMiddleware(app)
+        app_insights_logger().initRequestMiddleware(app)
+
+        logger(__name__).info("Starting hmpps-person-match-score using Python %s on %s" % (" ".join(sys.version.split(" ")[:1]), platform.platform()))
 
         app.config.from_mapping(
             SECRET_KEY='dev',
@@ -56,6 +41,6 @@ def create_app(test_config=None):
 
         return app
     except Exception as e:
-        ai_logger_instance.get_logger(__name__).exception('Exception on start up')
+        app_insights_logger().get_logger(__name__).exception('Exception on start up')
 
 
