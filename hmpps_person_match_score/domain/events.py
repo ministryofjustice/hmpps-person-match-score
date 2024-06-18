2a470b68aa685206af839443c22cd3f8e60908c0
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-06-18 14:54:39 +0100

    CPR-309 Revert event name

--- hmpps_person_match_score/domain/events.py
+++ hmpps_person_match_score/domain/events.py
@@ -3,6 +3,6 @@ class Events:
     App Insights Events
     """
     MATCH_SCORE_REQUESTED = "MatchScoreRequested"
-    MATCH_SCORE_GENERATED = "MatchScoreGenerated"
+    MATCH_SCORE_GENERATED = "PiCPersonMatchScoreGenerated"
     PERSON_MATCH_SCORE_REQUESTED = "PersonMatchScoreRequested"
     PERSON_MATCH_SCORE_GENERATED = "PersonMatchScoreGenerated"
