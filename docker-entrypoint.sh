ca9799d64d05cb844951722adf2f084480a8e280
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-08-30 10:29:57 +0100

    CPR-425 Increase Workers and Timeout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 source /opt/pysetup/.venv/bin/activate
 
-exec gunicorn --bind 0.0.0.0:5000 --forwarded-allow-ips='*' wsgi:app
+exec gunicorn --bind 0.0.0.0:5000 --forwarded-allow-ips='*' wsgi:app --workers 4 --timeout 600
