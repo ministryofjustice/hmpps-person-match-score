e7ff3e8e0a145800bab31c004fe3cc78fba198f4
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-11-01 14:22:51 +0000

    CPR-469 Fix test

--- hmpps_person_match_score/conftest.py
+++ hmpps_person_match_score/conftest.py
@@ -127,7 +127,7 @@ def mock_jwks_call_factory(jwks, requests_mock):
         """
         Mock call to JWKS endpoint.
         """
-        url = f"{os.environ.get("OAUTH_BASE_URL")}/.well-known/jwks.json"
+        url = f"{os.environ.get("OAUTH_BASE_URL")}/auth/.well-known/jwks.json"
 
         if headers is None:
             headers = {"Content-Type": "application/json"}
