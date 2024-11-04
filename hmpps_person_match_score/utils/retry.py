8eee7dd48246ee127702b9be671961fd04bc5711
Henry Iwanejko <henry.iwanejko@digital.justice.gov.uk>
2024-11-04 14:43:58 +0000

    CPR-469 Add retry method

---
+++ hmpps_person_match_score/utils/retry.py
@@ -0,0 +1,52 @@
+import random
+import time
+
+
+class RetryExecutor:
+    """
+    Class to execute potentially flaky functions
+    """
+
+    MAX_RETRY_COUNT = 3
+    MAX_DELAY_MS = 1000
+    DELAY_BASE_MS = 50
+
+    JITTER_MIN = 0.8
+    JITTER_MAX = 1.2
+
+    @classmethod
+    def retry(cls, func,
+              retry_exceptions: list[Exception] = None,
+              max_attempts: int = MAX_RETRY_COUNT,
+              base_delay: int = DELAY_BASE_MS,
+              max_delay: int = MAX_DELAY_MS,
+              ):
+        """
+        Retries a function with a jittered backoff strategy.
+
+        Args:
+            func: The function to be retried.
+            max_attempts: Maximum number of retry attempts.
+            base_delay: Base delay for exponential backoff (in seconds).
+            max_delay: Maximum delay for the backoff (in seconds).
+
+        Returns:
+            The result of the function if successful.
+
+        Raises:
+            Exception: Raises the last exception if all attempts fail.
+        """
+        if retry_exceptions is None:
+            retry_exceptions = (Exception,)
+
+        for attempt in range(max_attempts):
+            try:
+                return func()
+            except retry_exceptions as exception:
+                # If this is the last attempt, raise the exception
+                if attempt == max_attempts - 1:
+                    raise exception
+                # Calculate the delay with jitter
+                jitter = random.uniform(cls.JITTER_MIN, cls.JITTER_MAX)  # noqa: S311
+                delay = min(base_delay * pow(2, attempt), max_delay) * jitter
+                time.sleep(delay / 1000) # Convert to seconds
